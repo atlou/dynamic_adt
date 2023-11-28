6a53eac20ea4829059c59466ddbcb2ca182542ab
Xavier Normant <30378214+atlou@users.noreply.github.com>
2023-11-28 11:56:41 -0500

    Implemented removeExternal()

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -8,12 +8,12 @@ public class BinarySearchTree {
         public Node(int key, String value) {
             this.key = key;
             this.value = value;
-            this.left = new Node();
-            this.right = new Node();
+            this.left = new Node(key);
+            this.right = new Node(key);
         }
 
-        public Node() {
-            this.key = -1;
+        public Node(int key) {
+            this.key = key;
             this.value = null;
             this.left = null;
             this.right = null;
@@ -23,15 +23,74 @@ public class BinarySearchTree {
     private Node root;
 
     public BinarySearchTree() {
-        this.root = new Node();
+        this.root = new Node(0);
     }
 
     private boolean isExternal(Node node) {
-        return node.key == -1;
+        return node.left == null && node.right == null;
     }
 
     private boolean isRoot(Node node) {
         return node == this.root;
+    }
+
+    //--------------- ALLKEYS ---------------//
+    // TEST: allKeys
+    public void allKeys() {
+        System.out.println("ALL KEYS");
+        traversal(this.root);
+    }
+
+
+    private void traversal(Node node) {
+        if(!this.isExternal(node)) {
+            traversal(node.left);
+            System.out.println(node.key);
+            traversal(node.right);
+        }
+    }
+
+    //--------------- Removal ---------------//
+    public String remove(int key) {
+        System.out.println("Removing key " + key);
+        Node n = getNode(key);
+        if(isExternal(n.left)) {
+            removeExternal(n.left);
+        } else if(isExternal(n.right)) {
+            removeExternal(n.right);
+        } else {
+            System.out.println("Node has two inner children.");
+        }
+        return "";
+    }
+
+    // removes node and its parent
+    private void removeExternal(Node node) {
+        if(!this.isExternal(node)) {
+            System.out.printf("removeExternal fail: %d is not external.\n", node.key);
+            return;
+        }
+
+        // delete parent of external
+            // make grandparent link to the other child of parent
+        Node p = this.getParent(node);
+        if(p == null) {
+            System.out.printf("Could not find parent of %d.\n", node.key);
+            return;
+        }
+        Node gp = this.getParent(p);
+        if(gp == null) {
+            System.out.printf("Could not find parent of %d.\n", p.key);
+            return;
+        }
+
+        Node otherChild = p.left == node ? p.right : p.left;
+
+        if(gp.right == p) {
+            gp.right = otherChild;
+        } else {
+            gp.left = otherChild;
+        }
     }
 
     //--------------- Insertion ---------------//
@@ -42,8 +101,8 @@ public class BinarySearchTree {
         if(this.isExternal(n)) {
             n.key = key;
             n.value = value;
-            n.left = new Node();
-            n.right = new Node();
+            n.left = new Node(key);
+            n.right = new Node(key);
             return null;
         } else {
             // key already in tree
@@ -55,40 +114,36 @@ public class BinarySearchTree {
 
     //--------------- Parent ---------------//
     // TEST for parentSearch()
-    public int getParent(int key) {
-        Node p = searchParent(key, this.root);
+    public int getParentTest(int key) {
+        Node c = getNode(key);
+        Node p = searchParent(c, this.root);
         if(p != null) {
             return p.key;
         }
         return -2;
     }
 
-    private Node parent(Node node) {
-        return searchParent(node.key, this.root);
+    private Node getParent(Node child) {
+        return searchParent(child, this.root);
     }
 
     /*
-    returns the parent node of that key if it exists,
-    and returns null if it doesn't
+    returns the parent if it exists,
+    returns null otherwise
     */
-    private Node searchParent(int key, Node curr) {
-        if(this.isExternal(curr)) {
-            return null;
+    private Node searchParent(Node child, Node curr) {
+        if(curr.left == child || curr.right == child) {
+            return curr;
+        } else if(!this.isExternal(curr.left) && child.key < curr.key) {
+            return searchParent(child, curr.left);
+        } else if(!this.isExternal(curr.right) && child.key > curr.key) {
+            return searchParent(child, curr.right);
         }
 
-        if(curr.left.key == key || curr.right.key == key) {
-            return curr;
-        } else if(key < curr.key) {
-            return searchParent(key, curr.left);
-        } else {
-            return searchParent(key, curr.right);
-        }
+        return null;
     }
 
     //--------------- Search ---------------//
-
-
-
     private Node search(int key, Node curr) {
         if(this.isExternal(curr) || key == curr.key) {
             return curr;
@@ -99,7 +154,7 @@ public class BinarySearchTree {
         }
     }
 
-    public String get(int key) {
+    public String getValue(int key) {
         Node n = search(key, this.root);
         if(this.isExternal(n)) {
             return null;
@@ -107,8 +162,9 @@ public class BinarySearchTree {
         return n.value;
     }
 
-    //
-    // remove()
+    private Node getNode(int key) {
+        return search(key, this.root);
+    }
 
     //--------------- Next ---------------//
     public int nextKey(int key) {
