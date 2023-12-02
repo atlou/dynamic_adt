2965010acf71b0f5c9a3f4970d09e311304df73b
Xavier Normant <30378214+atlou@users.noreply.github.com>
2023-12-02 11:44:16 -0500

    comments

--- src/main/CleverSIDC.java
+++ src/main/CleverSIDC.java
@@ -4,20 +4,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class CleverSIDC {
 
-    private boolean largeData;
-    private int threshold;
-    private int count;
-    private LinkedList list;
-    private AVLTree avl;
+    private boolean largeData; // true when N has reached threshold, false otherwise
+    private int threshold; // number at which the data switches its underlying adt
+    private int count; // amount of data
+    private LinkedList list; // doubly linked list, used for small data amounts
+    private AVLTree avl; // avl tree, used for large data amounts
 
     public CleverSIDC() {
         this.largeData = false;
-        this.threshold = 10000;
+        this.threshold = 10000; // TODO: Find best threshold
         this.count = 0;
         this.list = new LinkedList();
         this.avl = new AVLTree();
     }
 
+    //------------ DYNAMIC ADT SWITCHING -------------//
     private void incrementCount() {
         this.count++;
         updateMode();
@@ -30,11 +31,13 @@ public class CleverSIDC {
 
     private void updateMode() {
         // small data goes over threshold
+        // switch to large data
         if(this.count >= this.threshold && !this.largeData) {
             this.largeData = true;
             this.listToAVL();
         }
         // large data goes under threshold
+        // switch to small data
         if(this.count < this.threshold && this.largeData) {
             this.largeData = false;
             this.avlToList();
@@ -69,15 +72,17 @@ public class CleverSIDC {
     // and vice-versa
     public void setSIDCThreshold(int threshold) {
         this.threshold = threshold;
+        this.updateMode();
     }
 
+    //------------ ADT METHODS -------------//
+
     // randomly generates new non-existing key of 8 digits
+    // bruteforce a new key
     public long generate() {
         int maxAttempts = 10000;
-        long min = 1;
-        long max = 10;
-//        long min = 10000000;
-//        long max = 100000000;
+        long min = 10000000;
+        long max = 100000000;
         long key;
         String v;
         int i = 0;
@@ -94,16 +99,18 @@ public class CleverSIDC {
         return key;
     }
 
-    // return all keys in sorted sequence
+    // return all keys in sorted sequence (array)
     public long[] allKeys() {
         return this.largeData ? this.avl.allKeys() : this.list.allKeys();
     }
 
     // add an entry for the given key and value
+    // if the key existed, its value is replaced and the old value is returned
     public String add(long key, String value) {
         String s = this.largeData ? this.avl.put(key, value) : this.list.put(key, value);
 
         if(s == null) {
+            // key does not already exist
             this.incrementCount();
         }
 
@@ -111,32 +118,37 @@ public class CleverSIDC {
     }
 
     // remove the entry for the given key
+    // if the key existed, its value is returned
     public String remove(long key) {
         String s = this.largeData ? this.avl.remove(key) : this.list.remove(key);
 
         if(s != null) {
+            // a key was removed
             this.decrementCount();
         }
 
         return s;
     }
 
-    // return the values of the given key
+    // return the value of the given key
     public String getValues(long key) {
         return this.largeData ? this.avl.getValue(key) : this.list.get(key);
     }
 
     // return the key for the successor of key
+    // i.e. the smallest key that is greater than the current key
     public long nextKey(long key) {
         return this.largeData ? this.avl.nextKey(key) : this.list.nextKey(key);
     }
 
     // return the key for the predecessor of key
+    // i.e. the biggest key that is smaller than the current key
     public long prevKey(long key) {
         return this.largeData ? this.avl.prevKey(key) : this.list.prevKey(key);
     }
 
     // returns the number of keys that are within the specified range of the two keys
+    // includes the bounds
     public int rangeKey(long key1, long key2) {
         return this.largeData ? this.avl.rangeKey(key1, key2) : this.list.rangeKey(key1, key2);
     }
