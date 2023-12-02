6828f3d98a1bbd6af8c5e001edb81ee9ca5e3527
Xavier Normant <30378214+atlou@users.noreply.github.com>
2023-12-01 21:33:40 -0500

    added attributes & implemented adt switching and insertions

--- src/main/CleverSIDC.java
+++ src/main/CleverSIDC.java
@@ -2,54 +2,117 @@ package main;
 
 public class CleverSIDC {
 
-    public CleverSIDC(int size) {
-        this.setSIDCThreshold(size);
+    private boolean largeData;
+    private int threshold;
+    private int count;
+    private LinkedList list;
+    private AVLTree avl;
+
+    public CleverSIDC() {
+        this.largeData = false;
+        this.threshold = 10000;
+        this.count = 0;
+        this.list = new LinkedList();
+        this.avl = new AVLTree();
+    }
+
+    private void incrementCount() {
+        this.count++;
+        updateMode();
+    }
+
+    private void decrementCount() {
+        this.count--;
+        updateMode();
+    }
+
+    private void updateMode() {
+        // small data goes over threshold
+        if(this.count >= this.threshold && !this.largeData) {
+            this.largeData = true;
+            this.listToAVL();
+        }
+        // large data goes under threshold
+        if(this.count < this.threshold && this.largeData) {
+            this.largeData = false;
+            this.avlToList();
+        }
+    }
+
+    private void listToAVL() {
+        System.out.println("Switching from Linked List to AVL Tree");
+        long[] keys = this.list.allKeys();
+        String[] data = this.list.allData();
+
+        for(int i = 0; i < keys.length; i++) {
+            this.avl.put(keys[i], data[i]);
+        }
+
+        this.list = new LinkedList();
+    }
+
+    private void avlToList() {
+        System.out.println("Switching from AVL Tree to Linked List");
+        long[] keys = this.avl.allKeys();
+        String[] data = this.avl.allData();
+
+        for(int i = 0; i < keys.length; i++) {
+            this.list.put(keys[i], data[i]);
+        }
+
+        this.avl = new AVLTree();
     }
 
     // where 100 ≤ Size ≤ ~500,000 is an integer number that defines
     // the size of the list. This size is very important as it will determine what data types or data
     // structures will be used (i.e. a Tree, Hash Table, AVL tree, binary tree, sequence, etc.)
-    private void setSIDCThreshold(int size) {
-
+    public void setSIDCThreshold(int threshold) {
+        this.threshold = threshold;
     }
 
     // randomly generates new non-existing key of 8 digits
-    public int generate() {
+    public long generate() {
         return 0;
     }
 
     // return all keys in sorted sequence
-    public int[] allKeys() {
-        return new int[0];
+    public long[] allKeys() {
+        return this.largeData ? this.avl.allKeys() : this.list.allKeys();
     }
 
     // add an entry for the given key and value
-    public void add(int key, String value) {
+    public String add(long key, String value) {
+        String s = this.largeData ? this.avl.put(key, value) : this.list.put(key, value);
 
+        if(s == null) {
+            this.incrementCount();
+        }
+
+        return s;
     }
 
     // remove the entry for the given key
-    public void remove(int key) {
-
+    public String remove(long key) {
+        return "";
     }
 
     // return the values of the given key
-    public String getValues(int key) {
+    public String getValues(long key) {
         return "";
     }
 
     // return the key for the successor of key
-    public int nextKey(int key) {
+    public int nextKey(long key) {
         return 0;
     }
 
     // return the key for the predecessor of key
-    public int prevKey(int key) {
+    public int prevKey(long key) {
         return 0;
     }
 
     // returns the number of keys that are within the specified range of the two keys
-    public int rangeKey(int key1, int key2) {
+    public int rangeKey(long key1, long key2) {
         return 0;
     }
 }
